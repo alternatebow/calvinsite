ca5781f02a52a4f524eed09d58267e25bd722f15
sensor47 <calslee2011@gmail.com>
2016-08-29 20:51:09 -0700

    js changes

--- webpage.js
+++ webpage.js
@@ -2,3 +2,8 @@ var $test = $('.foo');
 $test.on('click', function(){
 	$(this).html('<div style="color: red">something has changed here</div>')
 });
+
+var $starWars = $('.bar');
+$starWars.on('ready', function(){
+	$(this).fade('quick')
+});
