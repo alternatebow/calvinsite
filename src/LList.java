3186c8e4f321c148d866d338b692f503305ef6c3
jillHuang7 <yhuan437@mail.ccsf.edu>
2016-09-19 00:54:33 -0700

    Create LList.java
    Does the extra credit request mean this?  I am not quite sure..

---
+++ src/LList.java
@@ -0,0 +1,74 @@
+public class LList implements EntryWayListInterface{
+
+	public LList(){
+	}
+	
+	    /**
+     * Inserts an element to the beginging of the list.
+     * 
+     * @param newEntry
+     *            A parameter of generic type T that is the element to be
+     *            passed.
+     * @return Returns a boolean value true or false if the insertion was
+     *         successful or unable to respectively.
+     */
+    public boolean insertHead(T newEntry){
+    }
+
+    /**
+     * Inserts an element to the end of the list.
+     * 
+     * @param newEntry
+     *            A parameter of generic type T that is to be passed.
+     * @return Returns a true if element has successfully been added to end of
+     *         the list. Else it returns false.
+     */
+    public boolean insertTail(T newEntry){
+    }
+
+    /**
+     * Deletes the item at the beginning of the list and then returns that item.
+     * 
+     * @return Returns the item that was just removed from the list.
+     */
+    public T deleteHead(){
+    }
+
+    /**
+     * Deletes the item at the end of the list and returns it.
+     * 
+     * @return Returns said item that was at the end of the list.
+     */
+    public T deleteTail(){
+    }
+
+    /**
+     * Displays all elements currently on the list.
+     */
+    public void display(){
+    }
+
+    /**
+     * Checks to see if the list contains equal to the entry, and return true. False otherwise.
+     * 
+     * @param anEntry
+     *            Generic parameter that the methods checks for within the list.
+     * @return true if list contains an entry equal to what's passed in.
+     */
+    public boolean contains(T anEntry){
+    }
+
+    /**
+     *  Tests whether list is empty
+     * @return true if empty
+     */
+    public boolean isEmpty(){
+    }
+
+    /**
+     * Determines whether list is full.
+     * @return true if full
+     */
+    public boolean isFull(){
+    }
+}
