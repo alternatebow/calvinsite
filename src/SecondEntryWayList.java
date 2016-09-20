8dba0347622980992cb4ff7e958c26c9e3eedb3c
Karl Nicholas <knichola@mail.ccsf.edu>
2016-09-20 06:02:37 -0700

    Fix syntax errors.

--- src/SecondEntryWayList.java
+++ src/SecondEntryWayList.java
@@ -1,4 +1,5 @@
-public class SecondEntryWayList implements EntryWayListInterface { 
+
+public class SecondEntryWayList implements EntryWayListInterface {
 
 	private AList<T> aList;
 	private AList<T> internalList;
@@ -6,26 +7,26 @@ public class SecondEntryWayList implements EntryWayListInterface {
 	public SecondEntryWayList() {
 		this.internalList = new AList();
 	}
-    /**
-     * Inserts an element to the beginging of the list.
-     * 
-     * @param newEntry
-     *            A parameter of generic type T that is the element to be
-     *            passed.
-     * @return Returns a boolean value true or false if the insertion was
-     *         successful or unable to respectively.
-     */
-    public boolean insertHead(T newEntry){
-    	if (aList.isFull()) {
-    		return false;
-    		
-		else{
+
+	/**
+	 * Inserts an element to the beginging of the list.
+	 * 
+	 * @param newEntry
+	 *            A parameter of generic type T that is the element to be
+	 *            passed.
+	 * @return Returns a boolean value true or false if the insertion was
+	 *         successful or unable to respectively.
+	 */
+	public boolean insertHead(T newEntry) {
+		if (aList.isFull()) {
+			return false;
+		} else {
 			aList.add(1, newEntry);
 			return false;
 		}
-    }
+	}
 
-    /**
+	/**
      * Inserts an element to the end of the list.
      * 
      * @param newEntry
@@ -33,60 +34,71 @@ public class SecondEntryWayList implements EntryWayListInterface {
      * @return Returns a true if element has successfully been added to end of
      *         the list. Else it returns false.
      */
-    public boolean insertTail(T newEntry){
-    	if(aList.isFull()){
+    public boolean insertTail(T newEntry) {
+    	if(aList.isFull()) {
     		return false;
-    	}
-    	else{
-    		aList.add(aList.getLength+1,new Entry)
+    	} else {
+    		aList.add(aList.getLength+1,newEntry);
     		return true;
     	}
     }
 
-    /**
-     * Deletes the item at the beginning of the list and then returns that item.
-     * 
-     * @return Returns the item that was just removed from the list.
-     */
-    public T deleteHead(){
-    	if(aList.empty()){
-    	 return null;
-    	}else{
-    	return aList.remove(1);
-    	}
-    }
+	/**
+	 * Deletes the item at the beginning of the list and then returns that item.
+	 * 
+	 * @return Returns the item that was just removed from the list.
+	 */
+	public T deleteHead() {
+		if (aList.empty()) {
+			return null;
+		} else {
+			return aList.remove(1);
+		}
+	}
 
-    /**
-     * Deletes the item at the end of the list and returns it.
-     * 
-     * @return Returns said item that was at the end of the list.
-     */
-    public T deleteTail();
+	/**
+	 * Deletes the item at the end of the list and returns it.
+	 * 
+	 * @return Returns said item that was at the end of the list.
+	 */
+	public T deleteTail() {
 
-    /**
-     * Displays all elements currently on the list.
-     */
-    public void display();
+	}
 
-    /**
-     * Checks to see if the list contains equal to the entry, and return true. False otherwise.
-     * 
-     * @param anEntry
-     *            Generic parameter that the methods checks for within the list.
-     * @return true if list contains an entry equal to what's passed in.
-     */
-    public boolean contains(T anEntry);
+	/**
+	 * Displays all elements currently on the list.
+	 */
+	public void display() {
 
-    /**
-     *  Tests whether list is empty
-     * @return true if empty
-     */
-    public boolean isEmpty(){
+	}
 
-    }
+	/**
+	 * Checks to see if the list contains equal to the entry, and return true.
+	 * False otherwise.
+	 * 
+	 * @param anEntry
+	 *            Generic parameter that the methods checks for within the list.
+	 * @return true if list contains an entry equal to what's passed in.
+	 */
+	public boolean contains(T anEntry) {
 
-    /**
-     * Determines whether list is full.
-     * @return true if full
-     */
-    public boolean isFull();
+	}
+
+	/**
+	 * Tests whether list is empty
+	 * 
+	 * @return true if empty
+	 */
+	public boolean isEmpty() {
+
+	}
+
+	/**
+	 * Determines whether list is full.
+	 * 
+	 * @return true if full
+	 */
+	public boolean isFull() {
+
+	}
+}
